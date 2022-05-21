5200ed560de7f96c679b25a090f69de6495da45f
shailendra153 <kushwahshailendra12345@gmail.com>
2022-05-21 18:19:20 +0530

    'g'

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-
-app.use(express.static(__dirname + '/dist/pratispardha-frontend'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname ,'dist','pratispardha-frontend'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/pratispardha-frontend/index.html'));
+    res.sendFile(path.join(__dirname,'dist','pratispardha-frontend','index.html'));
 });
 
 // Start the app by listening on the default Heroku port
