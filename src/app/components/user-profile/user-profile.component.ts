d2565a4e384fa19e96baf4402ba4962ed4cd3e30
14shubh <burghateshubham115@gmail.com>
2022-06-28 14:22:32 +0530

    update signin page

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -73,6 +73,9 @@ export class UserProfileComponent implements OnInit {
       console.log("data is coming");
       console.log(data)
       this.UserProfile=data;
+      console.log("second data comming");
+      
+      console.log(this.UserProfile.image);
       this.name=this.UserProfile.name;
       this.email=this.UserProfile.email;
       this.mobile=this.UserProfile.mobile;
