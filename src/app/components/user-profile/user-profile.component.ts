229b2d08928180e2c4f20ee9196607531ca9de43
14shubh <burghateshubham115@gmail.com>
2022-06-28 17:36:37 +0530

    update user profile page

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -9,83 +9,92 @@ import { UserAuthService } from 'src/app/services/user-auth.service';
 export class UserProfileComponent implements OnInit {
   UserProfile: any;
   isDisabled: boolean = true;
-  name:string='';
-  email:string='';
-  mobile:string='';
-  age:string='';
-  description:string='';
-  playerType:string='';
-  address:string='';
-  image:any=undefined;
-  constructor(private _user:UserAuthService) {
+  name: string = '';
+  email: string = '';
+  mobile: string = '';
+  age: string = '';
+  description: string = '';
+  playerType: string = '';
+  address: string = '';
+  image: any = undefined;
+  constructor(private _user: UserAuthService) {
 
 
-   }
-   selectFile(){
+  }
+  selectFile() {
     console.log('selectFile');
     let element: HTMLElement = document.querySelector('input[type="file"]') as HTMLElement;
     element.click();
-   }
+  }
 
-   async uploadImage(event:any){
-     console.log("called.....................")
-      if(event.target.files.length > 0){
+  async uploadImage(event: any) {
+    console.log("called.....................")
+    if (event.target.files.length > 0) {
       const file = event.target.files[0];
       this.image = file;
-    console.log(this.image);
-        await this.update();
-    this.ngOnInit();
+      console.log(this.image);
+      await this.update();
+      this.ngOnInit();
     }
-   }
-   
-   
-  public status(isDisabled:any){
-    if(isDisabled){
+  }
+
+
+  public status(isDisabled: any) {
+    if (isDisabled) {
       this.isDisabled = false;
-    }else{
+    } else {
       this.isDisabled = true;
     }
-   }
-   update(){
-     let fd=new FormData();
-     fd.append("name",this.name);
-     fd.append("email",this.email);
-     fd.append("mobile",this.mobile);
-     fd.append("address",this.address);
-     fd.append("oldImage",this.UserProfile.image);
-     fd.append("age",this.age);
-     fd.append("image",this.image)
-     fd.append("description",this.description);
-     fd.append("playerType",this.playerType);
-     fd.append("playerId",this.UserProfile._id);
-     this._user.updateProfile(fd).subscribe(data=>{
-       console.log(data);
-       this.isDisabled=true;
-      
-       this.ngOnInit();
-     })
+  }
+  update() {
+    let fd = new FormData();
+    fd.append("name", this.name);
+    fd.append("email", this.email);
+    fd.append("mobile", this.mobile);
+    fd.append("address", this.address);
+    fd.append("oldImage", this.UserProfile.image);
+    fd.append("age", this.age);
+    fd.append("image", this.image)
+    fd.append("description", this.description);
+    fd.append("playerType", this.playerType);
+    fd.append("playerId", this.UserProfile._id);
+    this._user.updateProfile(fd).subscribe(data => {
+      console.log(data);
+      this.isDisabled = true;
 
-   }
+      this.ngOnInit();
+    })
+
+  }
 
 
   ngOnInit(): void {
-    this._user.viewProfile(sessionStorage.getItem('UserLoginId')).subscribe(data=>{
+    this._user.viewProfile(sessionStorage.getItem('UserLoginId')).subscribe(data => {
       console.log("data is coming");
       console.log(data)
-      this.UserProfile=data;
-      console.log("second data comming");
-      
+      this.UserProfile = data;
+      // console.log("second data comming");
+      // console.log(this.UserProfile); 
       console.log(this.UserProfile.image);
-      this.name=this.UserProfile.name;
-      this.email=this.UserProfile.email;
-      this.mobile=this.UserProfile.mobile;
-      this.address=this.UserProfile.address;
-      this.age=this.UserProfile.age;
-      this.playerType=this.UserProfile.playerType;
-      this.description=this.UserProfile.description;
+      this.name = this.UserProfile.name;
+      this.email = this.UserProfile.email;
+      this.mobile = this.UserProfile.mobile;
+      this.address = this.UserProfile.address;
+      this.age = this.UserProfile.age;
+      this.playerType = this.UserProfile.playerType;
+      this.description = this.UserProfile.description;
+
 
     })
+  }
 
+  public IsImage(){
+    if (this.UserProfile.image == ""){
+      return false;
+    }
+    else{
+      return true;
+    }
   }
 
 }
