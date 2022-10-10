86ea084b9b513741d119566dd49b15b0ef6174a7
shubham burghate <burghateshubham115@gmail.com>
2022-10-10 14:43:30 +0530

    add credential file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,10 @@ export class AppComponent implements OnInit{
   title = 'pratispardha_frontend';
   constructor(private router: Router,private toastr:ToastrService){}
   ngOnInit(): void {
-    // let router = this.router;  
-    // if(sessionStorage.getItem('jwt_token'))
-    //   this.toastr.error('Session is expired. Sign-in again');
-    
-    //   window.onload = function(){
-    //     //window.alert("oye.....")
-    //     sessionStorage.removeItem('jwt_token');
-        
-    //     router.navigate(['home']);
-    //   }
+    let router = this.router;
+    if(sessionStorage.getItem('jwt_token'))
+      window.onload = function(){
+        router.navigate(['home']);
+      }
   }
 }
